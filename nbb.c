51abeebe5a38e58e247a1cd96f2fd94471d1415a
Muzeen-Mir <112971539+Muzeen-Mir@users.noreply.github.com>
2023-01-05 06:08:35 +0000

    Thu, Jan 5, 2023, 6:08 AM +00:00

--- nbb.c
+++ nbb.c
@@ -1,19 +1,15 @@
-/*#include<cs50.h>
 #include<stdio.h>
+void meow(int n);
+
 int main(void)
 {
-    float regular_price = get_float("regular price:\t");
-    float sale = regular_price * .85;
-    printf("sale peice=%.2f\n",sale);
-}*/
-#include<stdio.h>
-#include<cs50.h>
-int main()
-{
-int the_number = get_int("number\t");
-    int number[100];
-for (int i=0; i<number; i++);
-{
-    printf("i");
+    meow(3);
 }
+    void meow(int n)
+
+{
+for (int i=0;i<n;i++)
+    {
+        printf("meow\n");
+    }
 }
