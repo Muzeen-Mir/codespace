c1def2a4359db35b064b80c457abc4a984f73795
Muzeen-Mir <112971539+Muzeen-Mir@users.noreply.github.com>
2023-01-05 06:07:40 +0000

    Thu, Jan 5, 2023, 6:07 AM +00:00

---
+++ nbb.c
@@ -0,0 +1,19 @@
+/*#include<cs50.h>
+#include<stdio.h>
+int main(void)
+{
+    float regular_price = get_float("regular price:\t");
+    float sale = regular_price * .85;
+    printf("sale peice=%.2f\n",sale);
+}*/
+#include<stdio.h>
+#include<cs50.h>
+int main()
+{
+int the_number = get_int("number\t");
+    int number[100];
+for (int i=0; i<number; i++);
+{
+    printf("i");
+}
+}
