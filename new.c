1dea6f93b5b59a468d97130f4dde7fa331ae1804
Muzeen-Mir <112971539+Muzeen-Mir@users.noreply.github.com>
2023-01-20 04:27:24 +0000

    Fri, Jan 20, 2023, 4:27 AM +00:00

--- new.c
+++ new.c
@@ -5,14 +5,16 @@ int main()
     printf("enter the number\n");
     scanf("%d%d",&a,&b);
     if(a>b)
-    (
+    {
         printf("a is greater than b");
-    )
-    else if(a=b)
-    (
+    }
+    else if(a==b)
+    {
         printf("a is equal to b");
-    )
+    }
     else
+    {
     printf("b is greater than a");
+    }
     return 0;
 }
